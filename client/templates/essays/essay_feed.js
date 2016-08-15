0ceffdf16ef81d25b150cac3f79f2fcd758a1a9b
wickerplane <wilgre@wharton.upenn.edu>
2016-08-15 01:36:12 -0400

    removed CSS

---
+++ client/templates/essays/essay_feed.js
@@ -0,0 +1,20 @@
+var postsData = [
+ 	{
+ 		title: 'Student #1',
+ 		url: 'http://upenn.edu',
+ 		essayNumber: 1
+	 },
+ 	{
+ 		title: 'Student #2',
+ 		url: 'http://ask.com',
+ 		essayNumber: 2
+ 	},
+ 	{
+ 		title: 'Student #3',
+ 		url: 'http://google.com'
+ 		essayNumber: 3
+ 	}
+];
+Template.postsList.helpers({
+ posts: postsData
+});
